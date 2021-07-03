b70a59923db8db95e91e0ac5db964c398bae696d
Bruno <brunosilvcarv@gmail.com>
2021-07-03 12:54:00 -0400

    lifecycle hooks in action

--- angular/complete-guide/cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ angular/complete-guide/cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,8 +1,16 @@
 import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
   Component,
+  DoCheck,
   Input,
+  OnChanges,
+  OnDestroy,
   OnInit,
-  ViewEncapsulation
+  SimpleChanges,
+  // ViewEncapsulation
 } from '@angular/core';
 
 @Component({
@@ -12,7 +20,15 @@ import {
   // encapsulation: ViewEncapsulation.None // takes off ViewEncapsulation
   // encapsulation: ViewEncapsulation.ShadowDom // uses the shadow dom simulation
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy {
   // tslint:disable-next-line:no-input-rename
   @Input('srvElement') // make it bindable from outside, pass data into this component
   element: {
@@ -20,10 +36,43 @@ export class ServerElementComponent implements OnInit {
     name: string,
     content: string
   };
+  @Input()
+  name: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log('Constructor called!');
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called!');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck called!');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit called!');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked called!');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called!');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked called!');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy called!');
+  }
 }
