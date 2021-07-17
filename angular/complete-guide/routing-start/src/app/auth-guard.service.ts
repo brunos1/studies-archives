318258829fcbb8761699b38c2f559cdbb1fa9456
Bruno <brunosilvcarv@gmail.com>
2021-07-17 10:54:16 -0400

    controlling navigation with canDeactivate

--- angular/complete-guide/routing-start/src/app/auth-guard.service.ts
+++ angular/complete-guide/routing-start/src/app/auth-guard.service.ts
@@ -9,7 +9,6 @@ export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot,
-              // @ts-ignore
               state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     return this.authService.isAuthenticated()
       .then(
