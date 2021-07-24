e261394f59c732fe1b41e52e774f0ac94f6f93a5
Bruno <brunosilvcarv@gmail.com>
2021-07-23 23:15:52 -0400

    using form data and reseting form

--- angular/complete-guide/forms-td-start/src/app/app.component.ts
+++ angular/complete-guide/forms-td-start/src/app/app.component.ts
@@ -12,6 +12,14 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
@@ -39,6 +47,14 @@ export class AppComponent {
   // }
 
   onSubmit() {
-    console.log(this.signupForm);
+    // console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secretQuestion;
+    this.user.answer = this.signupForm.value.answer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
