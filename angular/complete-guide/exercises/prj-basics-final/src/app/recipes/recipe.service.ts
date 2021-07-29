1cb169943582ac45261743ccb8dab5b40ade2912
Bruno <brunosilvcarv@gmail.com>
2021-07-28 21:27:11 -0400

    submitting the recipe edit form

--- angular/complete-guide/exercises/prj-basics-final/src/app/recipes/recipe.service.ts
+++ angular/complete-guide/exercises/prj-basics-final/src/app/recipes/recipe.service.ts
@@ -1,10 +1,13 @@
 import {Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Subject} from 'rxjs';
 import {Ingredient} from '../shared/ingredient.model';
 import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable()
 export class RecipeService {
+
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe(
@@ -37,5 +40,15 @@ export class RecipeService {
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
     this.shoppingListService.addIngredients(ingredients);
+  }
+
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipesChanged.next(this.recipes.slice());
   }
 }
