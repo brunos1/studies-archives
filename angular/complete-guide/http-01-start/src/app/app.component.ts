dfe21ffd97b6a786c79d8c0255fd1622276934cd
Bruno <brunosilvcarv@gmail.com>
2021-08-17 14:43:43 -0400

    firebase setup and post and get requests

---
+++ angular/complete-guide/http-01-start/src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    // console.log(postData);
+    this.http.post(
+      'https://ng-course-d3b5b-default-rtdb.firebaseio.com/posts.json',
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get(
+      'https://ng-course-d3b5b-default-rtdb.firebaseio.com/posts.json')
+      .subscribe(post => {
+      console.log(post);
+    });
+  }
+}
