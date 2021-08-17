f06a0a10dc68f66dcd43abd3d6baec88ed86ba1f
Bruno <brunosilvcarv@gmail.com>
2021-08-17 15:03:09 -0400

    observable operators to transform data

--- angular/complete-guide/http-01-start/src/app/app.component.ts
+++ angular/complete-guide/http-01-start/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { map } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -38,6 +40,15 @@ export class AppComponent implements OnInit {
   private fetchPosts() {
     this.http.get(
       'https://ng-course-d3b5b-default-rtdb.firebaseio.com/posts.json')
+      .pipe(map(responseData => {
+        const postsArray = [];
+        for (const key in responseData) {
+          if (responseData.hasOwnProperty(key)) {
+            postsArray.push({...responseData[key], id: key});
+          }
+        }
+        return postsArray;
+      }))
       .subscribe(post => {
       console.log(post);
     });
