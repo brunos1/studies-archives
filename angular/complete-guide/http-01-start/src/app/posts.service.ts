bf84596749a166a6162e0171e238f61278822695
Bruno <brunosilvcarv@gmail.com>
2021-09-01 12:12:39 -0400

    setting headers

--- angular/complete-guide/http-01-start/src/app/posts.service.ts
+++ angular/complete-guide/http-01-start/src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Post} from './post.model';
 import {map, catchError} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
@@ -26,7 +26,10 @@ export class PostsService {
 
   fetchPosts() {
     return this.http.get<{ [key: string]: Post }>(
-      'https://decent-envoy-231618-default-rtdb.firebaseio.com/posts.json')
+      'https://decent-envoy-231618-default-rtdb.firebaseio.com/posts.json',
+      {
+        headers: new HttpHeaders({'Custom-Header': 'Hello'})
+      })
       .pipe(
         map(responseData => {
           const postsArray: Post[] = [];
